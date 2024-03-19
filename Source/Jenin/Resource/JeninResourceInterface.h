1a33c6210d41aa8ef61ab26297ae69c95f1f867a
Adam Socki <socki.adam@gmail.com>
2024-03-18 21:13:23 -0500

    UI_SelectedUnits

---
+++ Source/Jenin/Resource/JeninResourceInterface.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "JeninResourceInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UJeninResourceInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class JENIN_API IJeninResourceInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
---
+++ Source/Jenin/UI/JeninUnitActionWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "JeninUnitActionWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JENIN_API UJeninUnitActionWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
